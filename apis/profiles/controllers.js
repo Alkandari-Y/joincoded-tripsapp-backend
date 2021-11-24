49b415fe6ae34dbc1505af4fb6dd7f13378a00c5
Alkandari-Y <yayalkandari@gmail.com>
2021-11-24 22:30:08 +0300

    updated controller for profile

--- apis/profiles/controllers.js
+++ apis/profiles/controllers.js
@@ -4,7 +4,7 @@ const Profile = require("../../db/models/Profile");
 //Find User
 exports.findUserProfileById = async (profileId, next) => {
   try {
-      const foundProfile = await Profile.findById(profileId);
+    const foundProfile = await Profile.findById(profileId);
       return foundProfile;
     } catch (error) {
         next(error);
@@ -38,10 +38,15 @@ exports.updateProfile = async (req, res, next) => {
     try {
       if (req.file) {
         req.body.image = `/media/${req.file.filename}`;
+        req.body.image = req.body.image.replace("\\", "/");
       };
-      console.log(req.body)
-      const updatedProfile = await req.profile.update(req.body);
-      return res.status(201).json(updatedProfile);
+      await Profile.findByIdAndUpdate(req.profile, req.body, {
+        new: true,
+        runValidators: true, // returns updated products
+      });
+      const foundProfile = await Profile.findOne({ user: req.user._id })
+      .populate({ path: "user", select: "username" })
+      return res.status(201).json(foundProfile);
     } catch (error) {
       return next(error);
     }
